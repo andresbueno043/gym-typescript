36335fd7cf10747795ee504bd414af0a9f6cfa04
andresbueno043 <ajbueno@iegabo.edu.co>
2023-04-11 12:44:00 -0500

    Navbar component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Navbar from "@/scenes/Navbar";
+import { useState } from "react";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
 
   return (
-    <div className="app">
-      app
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
