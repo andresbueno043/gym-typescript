8d0c0b7fc862b55a043daa2411185069bb200c25
andresbueno043 <ajbueno@iegabo.edu.co>
2023-04-11 13:08:04 -0500

    Selected Page enum created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Navbar from "@/scenes/Navbar";
 import { useState } from "react";
+import { SelectedPage } from "@/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
     <div className="app bg-gray-20">
